4391ecb060e6480dac52e07eef85e1ef42c0e7b5
specular-zxy <zhouxinyan33@gmail.com>
2020-04-22 11:42:25 +0800

    书上的代码，运行不通过？

--- A1010/main.cpp
+++ A1010/main.cpp
@@ -1,63 +1,82 @@
 #include <iostream>
-#include <string>
 #include <cstring>
-#include <cmath>
 #include <cstdio>
 using namespace std;
 
-int l = 35;
-int r = 2;
-bool error = false;
-int charge(char a[], int radix) {
-    int num = 0;
+typedef long long LL;
+LL Map[256];    // 0 ~ 9, a ~ z, 与 256的对应
+LL inf = (1LL << 63) - 1;
+
+void init() {
+    for (char c = '0'; c <= '9'; c++) {
+        Map[c] = c - '0';
+    }
+
+    for (char c = 'a'; c <= 'z'; c++) {
+        Map[c] = c - 'a' + 10;
+    }
+}
+
+LL convertNum10(char a[], LL radix, LL t) {
+    LL ans = 0;
     int len = strlen(a);
-    char c;
     for (int i = 0; i < len; i++) {
-        c = a[len - i - 1];
-        if (c >= '0' && c <= '9') {
-            num += (c - '0') * pow(radix, i);
-        }else if (c >= 'a' && c <= 'z') {
-            if (radix < (c - 'a' + 10)){error  = true; return -1;}
-            num += (c - 'a' + 10) * pow(radix, i);
-        }
+        ans = ans * radix + Map[a[i]];
+        if (ans < 0 || ans > t) return -1;
     }
-    return num;
+    return ans;
 }
-int binarysearch(int key, char b[]){
-    int mid = (l + r) / 2;
-    int ret = charge(b, mid);
 
-    while (ret != key) {
-        if (ret > mid) l = mid - 1;
-        else if (ret < mid) r = mid + 1;
-        mid = (l + r) / 2;
-        ret = charge(b, mid);
-        if (ret == -1)
-            return -1;
+int cmp(char N2[], LL radix, LL t) {
+    int len = strlen(N2);
+    LL num = convertNum10(N2, radix, t);
+
+    if (num < 0) return 1;
+    if (t > num) return -1;
+    else if (t == num) return 0;
+    else return 1;
+}
+
+LL binarySearch(char N2[], LL left, LL right, LL t) {
+    LL mid;
+    while (left <= right) {
+        mid = (left + right) / 2;
+        int flag = cmp(N2, mid, t);
+        if (flag == 0) return mid;
+        else if (flag == -1) return left = mid + 1;
+        else right = mid - 1;
     }
-
-    return mid;
+    return -1;
 }
+
+int findLargestDigit(char N2[]) {
+    int ans = -1, len = strlen(N2);
+    for (int i = 0; i < len; i++) {
+        if (Map[N2[i]] > ans)
+            ans = Map[N2[i]];
+    }
+    return ans + 1;
+}
+
+char N1[20], N2[20], temp[20];
+int tag, radix;
 
 int main() {
-    char n1[12] = "0";
-    char n2[12] = "0";
-    int tag, radix;
-    scanf("%s %s %d %d", n1, n2, &tag, &radix);
-    int num;
-    int result;
-    if (tag == 1) {                  // radix is for n1
-        num = charge(n1, radix);
-        result = binarysearch(num, n2);
-    } else if (tag == 2) {           // radix is for n2
-        num = charge(n2, radix);
-        result = binarysearch(num, n1);
+    init();
+    scanf("%s %s %d %d", N1, N2, tag, radix);
+
+    if (tag == 2) {
+        strcpy(temp, N1);
+        strcpy(N1, N2);
+        strcpy(N2, temp);
     }
 
-    if (result == -1 && error) {
-        printf("Impossible\n");
-    } else
-        printf("%d", result);
+    LL t = convertNum10(N1, radix, inf);
+    LL low = findLargestDigit(N2);
+    LL high = max(low, t) + 1;
+    LL ans = binarySearch(N2, low, high, t);
+    if (ans == -1) printf("Impossible\n");
+    else printf("%lld\n", ans);
 
     return 0;
 }
