dda9a3edab483488005384955583a0def77d3900
specular-zxy <zhouxinyan33@gmail.com>
2020-04-22 11:53:30 +0800

    correct answer

--- A1010/main.cpp
+++ A1010/main.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <cmath>
 using namespace std;
+// 进制转换
 long long convert(string n, long long radix) {
     long long sum = 0;
     int index = 0, temp = 0;
@@ -12,24 +13,26 @@ long long convert(string n, long long radix) {
     }
     return sum;
 }
-long long find_radix(string n, long long num) {
+
+// 查找
+long long find_radix(string n, long long num) {    //num radix
     char it = *max_element(n.begin(), n.end());
     long long low = (isdigit(it) ? it - '0': it - 'a' + 10) + 1;
     long long high = max(num, low);
     while (low <= high) {
         long long mid = (low + high) / 2;
         long long t = convert(n, mid);
-        if (t < 0 || t > num) high = mid - 1;
-        else if (t == num) return mid;
+        if (t < 0 || t > num) high = mid - 1;    // t = convert number
+        else if (t == num) return mid;           // 找到了
         else low = mid + 1;
     }
     return -1;
 }
 int main() {
     string n1, n2;
-    long long tag = 0, radix = 0, result_radix;
+    long long tag , radix = 0, result_radix;
     cin >> n1 >> n2 >> tag >> radix;
-    result_radix = tag == 1 ? find_radix(n2, convert(n1, radix)) : find_radix(n1, convert(n2, radix));
+    result_radix = (tag == 1) ? find_radix(n2, convert(n1, radix)) : find_radix(n1, convert(n2, radix));
     if (result_radix != -1) {
         printf("%lld", result_radix);
     } else {
