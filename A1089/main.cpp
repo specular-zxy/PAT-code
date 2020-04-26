82d187dd17b3fa4fbbb1fda2b318c618f553e606
specular-zxy <zhouxinyan33@gmail.com>
2020-04-26 11:45:01 +0800

    2020.04.26

--- A1089/main.cpp
+++ A1089/main.cpp
@@ -1,76 +1,39 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int origin[111], tempOri[111], changed[111];
-int n;
-
-bool issame(int a[], int b[]) {
-    for (int i = 0; i < n; i++) {
-        if (a[i] != b[i]) return false;
-    }
-    return true;
-}
-
-void showArray(int a[]) {
-    for (int i = 0; i < n; i++) {
-        printf("%d", a[i]);
-        if (i != n - 1) printf(" ");
-    }
-}
-bool insersort() {
-    bool flag = true;
-    for (int i = 1; i < n; i++) {
-        if (i != 1 && issame(tempOri, changed)) {
-            flag = true;
-        }
-
-        int tmp = tempOri[i], j = 1;
-        while (j > 0 && tempOri[j - 1] > tmp) {
-            tempOri[j] = tempOri[j - 1];
-            j--;
-        }
-        tempOri[j] = tmp;
-        if (flag == true)
-            return true;
-    }
-    return false;
-}
-
-void mergesort() {
-    bool flag = false;
-    for (int step = 2; step /2 <= n; step *= 2) {
-        if (step != 2 && issame(tempOri, changed)) {
-            flag = true;
-        }
-
-        for (int i = 0; i < n; i += step) {
-            sort(tempOri + i, tempOri + min(i + step, n));
-        }
-
-        if (flag) {
-            showArray(tempOri);
-            return;
-        }
-    }
-}
 int main() {
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &origin[i]);
-        tempOri[i] = origin[i];
-    }
-
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &changed[i]);
-    }
-
-    if (insersort()) {
-        printf("Insertion Sort\n");
-        showArray(tempOri);
+    int n, a[100], b[100], i, j;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < n; i++)
+        cin >> b[i];
+    // 对于insertsort进行比较
+    for (i = 0; i < n - 1 && b[i] <= b[i + 1]; i++);
+    for (j = i + 1; a[j] == b[j] && j < n; j++);
+    // 为insertsort，然后再sort一步
+    if (j == n) {
+        cout << "Insertion Sort" << endl;
+        sort(a, a + i + 2);
     } else {
-        printf("Merge Sort\n");
-        for (int i = 0; i < n; i++)
-            tempOri[i] = origin[i];
-        mergesort();
+        cout << "Merge Sort" << endl;
+        int k = 1, flag = 1;
+        // merge sort
+        while(flag) {
+            flag = 0;
+            for (i = 0; i < n; i++) {
+                if (a[i] != b[i])
+                    flag = 1;
+            }
+            k = k * 2;
+            for (i = 0; i < n / k; i++)
+                sort(a + i * k, a + (i + 1) * k);
+            sort(a + n / k * k, a + n);
+        }
     }
+    for (j = 0; j < n; j++) {
+        if (j != 0) printf(" ");
+        printf("%d", a[j]);
+    }
+    return 0;
 }
